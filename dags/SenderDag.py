6b46370014c05cafba1a0318f90a988c39bd8951
Selcan Yağmur Atak <selcanyagmuratak@crypttech.com>
2023-02-01 17:57:26 +0300

    feature(dags): change code format to execute 3 different api data
    Get location ids, loop over them and send their weather data to Kafka topic partitions

--- dags/SenderDag.py
+++ dags/SenderDag.py
@@ -16,21 +16,26 @@ from src.utils import get_api_data, send_data
 )
 def generate_data_etl():
 
+    location_list = [100323786, 100745044, 100311046]
+
     # @task.sensor
     # def wait_initialization():
     #     return ExternalTaskSensor(task_id="wait_upstream", external_dag_id="init_process", external_task_id=None)
 
-    @task
-    def get_weather_api_data():
-        print("in weather api")
-        return get_api_data()
+    # @task
+    # def get_weather_api_data():
+    #     print("in weather api")
+    #     return get_api_data()
 
     @task
-    def send_to_kafka(obj):
-        print(f"kafka data: {obj}")
-        send_data(data=obj)
+    def send_to_kafka():
+        # print(f"kafka data: {obj}")
+        for p, location in enumerate(location_list):
+            data = get_api_data(location_id=location)
+            print(f"api data: {data} partition: {p}")
+            send_data(loc_id=location, data=data, partition=p)
 
-    send_to_kafka(get_weather_api_data())
+    send_to_kafka()
 
 
 data_send_dag = generate_data_etl()
