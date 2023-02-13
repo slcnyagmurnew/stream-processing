d51bf618912a704d7f884768935706d3cd15145d
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-02-13 18:26:39 +0300

    feat(dags): change task format because api data will not be used anymore
    Use log data and create its necessary operations. Update data to Redis and Kafka functions. Update config file for each new data

--- dags/SenderDag.py
+++ dags/SenderDag.py
@@ -1,49 +1,52 @@
-import time
+from datetime import timedelta
 import pendulum
-from src.utils import get_api_data
+from src.utils import get_log_data, get_config, update_config
 from src.ops import redis_connection, send_to_redis, send_to_kafka
 from airflow.operators.python import PythonOperator
 from airflow import DAG
 
+BASE_CONF_DIR = "/opt/airflow/dags"
 
 with DAG(
-    "generate_data",
-    schedule_interval="* * * * *",
-    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
-    catchup=False,
-    is_paused_upon_creation=False
+        "generate_data_for_stream_processing",
+        schedule_interval=timedelta(seconds=20),
+        start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+        catchup=False,
+        is_paused_upon_creation=False
 ) as dag:
-    location_list = {"ankara": 100323786, "istanbul": 100745044, "izmir": 100311046}  # to config maybe
-    current_data = []
-    for location in list(location_list.values()):
-        current_data.append(get_api_data(location_id=location))
-        time.sleep(0.5)
+    conf = get_config()
 
-    check_redis_connection = PythonOperator(
+    current_data = get_log_data(conf["dag_configs"]["last_processed_file"])
+
+    check_redis_connection_task = PythonOperator(
         python_callable=redis_connection,
         task_id="check_redis_connection",
         dag=dag
     )
 
-    generate_data_to_kafka = PythonOperator(
+    generate_data_to_kafka_task = PythonOperator(
         task_id="send_data_to_kafka",
         python_callable=send_to_kafka,
         op_kwargs={
-            "location_data": current_data,
-            "location_lst": location_list
+            "data": current_data
         },
         dag=dag
     )
 
-    generate_data_to_redis = PythonOperator(
+    generate_data_to_redis_task = PythonOperator(
         task_id="send_data_to_redis",
         python_callable=send_to_redis,
         op_kwargs={
-            "location_data": current_data,
-            "location_lst": location_list
+            "data": current_data,
+            "data_type": "unq_dst_ip"
         },
         dag=dag
     )
 
-    check_redis_connection >> [generate_data_to_kafka, generate_data_to_redis]
+    update_config_task = PythonOperator(
+        task_id="update_config_file",
+        python_callable=update_config,
+        dag=dag
+    )
 
+    check_redis_connection_task >> [generate_data_to_kafka_task, generate_data_to_redis_task] >> update_config_task
