497648314f718a1779d8aeb6978ce0cbe97a9690
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-02-13 18:24:25 +0300

    feat(src): change data format due to using of api data no more
    Create new struct type in a proper way with log data. Subscribe new Kafka topic for streaming of aggregation of log data

--- src/stream.py
+++ src/stream.py
@@ -1,19 +1,11 @@
-# from KafkaAdapter import KafkaAdapter
 import os
-# import logging
-# import requests
-# from pyspark import SparkContext
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType
-# from pyspark.streaming import StreamingContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import from_json, col, avg, to_json
 
-# adapter = KafkaAdapter()
-# BOOTSTRAP_SERVERS = os.getenv("BOOTSTRAP_SERVERS", default=["kafka:9092"])
-SPARK_KAFKA_SERVER = "kafka-server:9092"
-# SPARK_KAFKA_SERVER = os.getenv("SPARK_KAFKA_SERVER", default="kafka:9092")
-# TOPIC_NAME = os.getenv("TOPIC_NAME", default=None)
-TOPIC_NAME = "WeatherTopic"
+SPARK_KAFKA_SERVER = os.getenv("SPARK_KAFKA_SERVER", default="kafka:9092")
+SPARK_KAFKA_TOPIC = os.getenv("SPARK_KAFKA_TOPIC", default=None)
+TOPIC_NAME = os.getenv("TOPIC_NAME", default=None)
 
 """
     .config("spark.cassandra.connection.host", "cassandra") \
@@ -26,29 +18,30 @@ spark = SparkSession \
     .builder \
     .appName('StreamProcessingDemo').getOrCreate()
 
-
 struct = StructType(
     [
-        StructField("time", StringType()),
-        StructField("temperature", DoubleType()),
-        StructField("feelsLikeTemp", DoubleType()),
-        StructField("windSpeed", DoubleType()),
-        StructField("windDir", DoubleType()),
-        StructField("pressure", DoubleType()),
-        StructField("symbolPhrase", StringType())
+        StructField("src_ip", StringType()),
+        StructField("timestamp", StringType()),
+        StructField("unq_dst_ip", StringType()),
+        StructField("allow", StringType()),
+        StructField("drop", StringType()),
+        StructField("frequency", StringType()),
+        StructField("pkts_sent", StringType()),
+        StructField("pkts_received", StringType()),
+        StructField("bytesin", StringType()),
+        StructField("bytesout", StringType()),
+        StructField("unq_dst_port", StringType())
     ]
 )
 
 
 def read_stream_from_kafka():
-    print(SPARK_KAFKA_SERVER)
-    print(TOPIC_NAME)
     sdf = spark \
         .readStream \
         .format("kafka") \
         .option("kafka.bootstrap.servers", SPARK_KAFKA_SERVER) \
+        .option("group.id", "SparkStreamGroup") \
         .option("subscribe", TOPIC_NAME) \
-        .option("startingOffsets", "earliest") \
         .option("delimiter", ",") \
         .load()
 
@@ -56,24 +49,54 @@ def read_stream_from_kafka():
 
     sdf.printSchema()
 
-    df1 = sdf.selectExpr("CAST(value AS STRING)").select(from_json(col("value"), struct).alias("data")).select(
-        "data.*")
-    df1.printSchema()
-    df1.writeStream \
+    # It is necessary that convert data as string format for using in expressions
+    sdf = sdf.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+        .select(from_json(col("value"), struct)  # get all structured data into dataframe
+                .alias("data"))  # access column names with data.$column_name
+
+    sdf.printSchema()
+
+    sdf = sdf.groupby("data.src_ip").agg(avg("data.unq_dst_ip").alias("value"))  # group by with column
+
+    sdf.printSchema()
+
+    """
+    Write key-value data from a DataFrame to a specific Kafka topic specified in an option
+    Use "key" word to set "value" data as Kafka key field
+    "value" keyword is required to run query, it searches for "value" column
+    """
+    sdf.select(col("src_ip").alias("key"), col("value").alias("value").cast("string"))  \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", SPARK_KAFKA_SERVER) \
+        .option("topic", SPARK_KAFKA_TOPIC) \
+        .option("checkpointLocation", "/tmp/checkpoint") \
+        .trigger(processingTime='10 minutes') \
         .outputMode("update") \
-        .format("console") \
-        .option("truncate", False) \
         .start() \
         .awaitTermination()
 
+    """ DIRECT WRITE TO CONSOLE """
+    # df1.writeStream \
+    #     .outputMode("update") \
+    #     .format("console") \
+    #     .option("truncate", False) \
+    #     .start() \
+    #     .awaitTermination()
 
-# def read_batch_from_kafka():
-#     bdf = spark \
-#         .read \
-#         .format('kafka') \
-#         .option('kafka.bootstrap.servers', BOOTSTRAP_SERVERS) \
-#         .option('subscribe', TOPIC_NAME) \
-#         .load()
+    """TO CONSOLE WITH 10 MINI BATCHING"""
+    # df1.writeStream \
+    #     .format("console") \
+    #     .outputMode("update") \
+    #     .trigger(processingTime='10 minutes') \
+    #     .start() \
+    #     .awaitTermination()
+
+    """WRITE TO REDIS"""
+    # df1.writeStream \
+    #     .foreachBatch(foreach_batch_to_redis) \
+    #     .start() \
+    #     .awaitTermination()
 
 
 def write_to_cassandra(write_df, _):
